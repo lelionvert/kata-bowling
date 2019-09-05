9987206989914ed3748be038ac3b3e750743d2da
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-09-05 09:53:15 +0200

    refacto Score calculate points

---
+++ src/main/java/Score.java
@@ -0,0 +1,6 @@
+class Score {
+
+    static int calculatePoints(int numberOfPins) {
+        return numberOfPins;
+    }
+}
---
+++ src/test/java/BowlingTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BowlingTest {
+
+    @Test
+    public void when_a_number_of_pins_fail_we_win_the_same_number_points() {
+        int numberOfPins = 5;
+        int expectedNumberOfPoints = 5;
+        assertThat(expectedNumberOfPoints).isEqualTo(Score.calculatePoints(numberOfPins));
+    }
+
+
+}
